76633f797f8eae50162e71f183c5d4b8cfc2c0e3
chtholine <oleg123qwe@gmail.com>
2024-04-19 16:30:44 +0300

    refactor

--- main.py
+++ main.py
@@ -4,7 +4,119 @@ from PyQt6 import QtCore, QtWidgets
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QApplication, QFileDialog, QMainWindow
-from stegano import lsb
+# Python program implementing Image Steganography
+
+# PIL module is used to extract
+# pixels of image and modify it
+from PIL import Image
+
+
+# Convert encoding data into 8-bit binary
+# form using ASCII value of characters
+def genData(data):
+    # list of binary codes
+    # of given data
+    newd = []
+
+    for i in data:
+        newd.append(format(ord(i), '08b'))
+    return newd
+
+
+# Pixels are modified according to the
+# 8-bit binary data and finally returned
+def modPix(pix, data):
+    datalist = genData(data)
+    lendata = len(datalist)
+    imdata = iter(pix)
+
+    for i in range(lendata):
+
+        # Extracting 3 pixels at a time
+        pix = list(
+            imdata.__next__()[:3]
+            + imdata.__next__()[:3]
+            + imdata.__next__()[:3]
+        )
+
+        # Pixel value should be made
+        # odd for 1 and even for 0
+        for j in range(0, 8):
+            if datalist[i][j] == '0' and pix[j] % 2 != 0:
+                pix[j] -= 1
+
+            elif datalist[i][j] == '1' and pix[j] % 2 == 0:
+                if pix[j] != 0:
+                    pix[j] -= 1
+                else:
+                    pix[j] += 1
+                # pix[j] -= 1
+
+        # Eighth pixel of every set tells
+        # whether to stop ot read further.
+        # 0 means keep reading; 1 means thec
+        # message is over.
+        if (i == lendata - 1):
+            if (pix[-1] % 2 == 0):
+                if (pix[-1] != 0):
+                    pix[-1] -= 1
+                else:
+                    pix[-1] += 1
+
+        else:
+            if (pix[-1] % 2 != 0):
+                pix[-1] -= 1
+
+        pix = tuple(pix)
+        yield pix[:3]
+        yield pix[3:6]
+        yield pix[6:9]
+
+
+def encode_enc(newimg, data):
+    w = newimg.size[0]
+    (x, y) = (0, 0)
+
+    for pixel in modPix(newimg.getdata(), data):
+
+        # Putting modified pixels in the new image
+        newimg.putpixel((x, y), pixel)
+        if (x == w - 1):
+            x = 0
+            y += 1
+        else:
+            x += 1
+
+
+# Encode data into image
+def encode(path: str, output_path: str, message: str):
+    image = Image.open(path, 'r')
+
+    newimg = image.copy()
+    encode_enc(newimg, message)
+
+    return newimg.save(output_path)
+
+
+# Decode the data in the image
+def decode(path: str):
+    image = Image.open(path, 'r')
+
+    data = ''
+    imgdata = iter(image.getdata())
+
+    while True:
+        pixels = list(
+            imgdata.__next__()[:3]
+            + imgdata.__next__()[:3]
+            + imgdata.__next__()[:3]
+        )
+
+        binstr = ''.join('0' if (i % 2 == 0) else '1' for i in pixels[:8])
+        data += chr(int(binstr, 2))
+        if (pixels[-1] % 2 != 0):
+            return data
+
 
 
 class UI(QMainWindow):
@@ -151,7 +263,7 @@ class UI(QMainWindow):
 
     def browse_output_path(self):
         # Set file filter to enforce PNG files only
-        save_path, _ = QFileDialog.getSaveFileName(self, "Save File", "", "PNG Images (*.png)")
+        save_path, _ = QFileDialog.getSaveFileName(self, "Save File", "", "Images (*.png *.jpg *.jpeg)")
         self.output_path.setText(save_path)
 
         # Enforce PNG extension if user doesn't provide it
@@ -173,8 +285,7 @@ class UI(QMainWindow):
 
         if input_path and message:
             try:
-                secret = lsb.hide(input_path, message)
-                secret.save(output_path)
+                encode(input_path, output_path, message)
                 self.handling_label.setText("Message encoded successfully!")
             except Exception as e:
                 self.handling_label.setText(f"Error: {str(e)}")
@@ -182,11 +293,9 @@ class UI(QMainWindow):
             self.handling_label.setText("Error: Input path or message is empty!")
 
     def decode_message(self):
-        input_path = self.input_path.text()
-
-        if input_path:
+        if input_path := self.input_path.text():
             try:
-                show_message = lsb.reveal(input_path)
+                show_message = decode(input_path)
                 self.textEdit.setPlainText(show_message)
                 self.handling_label.setText("Message decoded successfully!")
             except Exception as e:
